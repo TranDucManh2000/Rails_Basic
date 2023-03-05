ad12ed602656229c1c00d89353dbbb39f0f37a8a
Manhtd <manh0982880741@gmail.com>
2023-03-05 14:36:56 +0700

    add destroy table

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :user
-  has_one :parent
+  has_one :parent,dependent: :destroy
   accepts_nested_attributes_for :parent
   validates :name,:gender,:class_name,presence: true
   validates :age,numericality: { only_integer: true }
